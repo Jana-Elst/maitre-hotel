d2bdc61ee4bac3f0db649a699296a197ba486c6f
Jana Elst <jana.elst@student.howest.be>
2025-03-26 16:20:46 +0100

    /

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,16 @@
 import { productData } from "./data";
 
+const isEmpty = (object) => {
+    for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 const createBill = (restaurantVariables) => {
     console.log('restVARS', restaurantVariables);
     const tableId = restaurantVariables.activeState.tableId;
@@ -10,35 +21,37 @@ const createBill = (restaurantVariables) => {
         const orders = restaurantVariables.orders;
 
         //welke order items zitten er in de bill?
-        let itemsBill = [];
-        bill.orders.forEach(orderId => {
-            orders.forEach(order => {
-                if (orderId === order.id) {
-                    itemsBill.push(order.items);
-                }
-            });
-        });
-
-        console.log('itemsBill', itemsBill.flat(Infinity));
-
-        //items met zelfde productId samenvoegen
-        let mergedOrdersBill = [];
-        itemsBill.flat(Infinity).forEach(item => {
-            if (mergedOrdersBill.some(i => i.productId === item.productId)) {
-                mergedOrdersBill.forEach(i => {
-                    if (i.productId === item.productId) {
-                        i.amount += item.amount
+        if (bill) {
+            let itemsBill = [];
+            bill.orders.forEach(orderId => {
+                orders.forEach(order => {
+                    if (orderId === order.id) {
+                        itemsBill.push(order.items);
                     }
                 });
-            } else {
-                mergedOrdersBill.push({
-                    productId: item.productId,
-                    amount: item.amount
-                })
-            }
-        })
-        console.log('mergedOrders', mergedOrdersBill);
-        return mergedOrdersBill;
+            });
+
+            console.log('itemsBill', itemsBill.flat(Infinity));
+
+            //items met zelfde productId samenvoegen
+            let mergedOrdersBill = [];
+            itemsBill.flat(Infinity).forEach(item => {
+                if (mergedOrdersBill.some(i => i.productId === item.productId)) {
+                    mergedOrdersBill.forEach(i => {
+                        if (i.productId === item.productId) {
+                            i.amount += item.amount
+                        }
+                    });
+                } else {
+                    mergedOrdersBill.push({
+                        productId: item.productId,
+                        amount: item.amount
+                    })
+                }
+            })
+            console.log('mergedOrders', mergedOrdersBill);
+            return mergedOrdersBill;
+        }
     }
 }
 
